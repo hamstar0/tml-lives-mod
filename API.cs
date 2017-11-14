45b0e72127cd7fdfed32e1b348be3119b72a822c
hamstar <webutek@gmail.com>
2017-11-14 13:22:50 -0800

    v1.5.5.2 * Expanded API

--- API.cs
+++ API.cs
@@ -1,7 +1,32 @@
-﻿namespace Lives {
+﻿using Terraria;
+
+namespace Lives {
 	public static class LivesAPI {
 		public static LivesConfigData GetModSettings() {
 			return LivesMod.Instance.Config.Data;
+		}
+
+
+		public static bool AddLives( Player player, int lives ) {
+			var myplayer = player.GetModPlayer<MyPlayer>();
+			return myplayer.AddLives( lives );
+		}
+		
+		public static int GetLives( Player player ) {
+			var myplayer = player.GetModPlayer<MyPlayer>();
+			return myplayer.Lives;
+		}
+		public static int GetDeaths( Player player ) {
+			var myplayer = player.GetModPlayer<MyPlayer>();
+			return myplayer.Deaths;
+		}
+		public static byte GetOriginalDifficulty( Player player ) {
+			var myplayer = player.GetModPlayer<MyPlayer>();
+			return myplayer.OriginalDifficulty;
+		}
+		public static bool IsImmorta( Player player ) {
+			var myplayer = player.GetModPlayer<MyPlayer>();
+			return myplayer.IsImmortal;
 		}
 	}
 }
